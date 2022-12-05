f158423e23525068f2da719c29333bf67150e1d0
Takayyz <like.the.wind0306@gmail.com>
2022-12-05 16:32:44 +0900

    feat: APIルートのサンプル

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+type HelloResponse = {
+  name: string;
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const Sample = (req: NextApiRequest, res: NextApiResponse<HelloResponse>) => {
+  res.status(200).json({ name: 'Taka' });
+};
+
+export default Sample;
+
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
