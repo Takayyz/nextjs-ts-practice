31225008918a282679f7fc47366bfe477fc3efc9
Takayyz <like.the.wind0306@gmail.com>
2022-12-05 19:35:36 +0900

    feat: addon-actionsで任意のデータをActionsで表示

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import { ComponentMeta } from '@storybook/react';
 import { StyledButton } from '../components/StyledButton';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'StyledButton',
   component: StyledButton,
+  // argTypes: {
+  //   onClick: {action: 'clicked'}
+  // },
 } as ComponentMeta<typeof StyledButton>;
 
+const incrementAction = action('increment');
+
 export const Primary = (props) => {
+  const [count, setCount] = useState(0);
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count);
+    setCount((c) => c + 1);
+  };
+
   return (
-    <StyledButton {...props} variant="primary">Primary</StyledButton>
+    <StyledButton {...props} variant="primary" onClick={onClick}>Count: {count}</StyledButton>
   );
+  // return (
+  //   <StyledButton {...props} variant="primary">Primary</StyledButton>
+  // );
 };
 
 export const Success = (props) => {
